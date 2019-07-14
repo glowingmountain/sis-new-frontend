add0fadad2875ada1b39be3e6b284b3116caec47
glowingmountain <glowingmountain@gmail.com>
2019-07-14 02:42:35 -0600

    routing, header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    routingComponents,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
